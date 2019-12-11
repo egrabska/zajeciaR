9eabd2599acca78bd6108d8d5efc4663e47079de
Ewa Grabska <egrabska@PC-0001.ur.local>
2019-12-11 16:22:16 +0100

    changes wednesday

--- skrypt1.R
+++ skrypt1.R
@@ -29,7 +29,9 @@ summary(dane)
 
 #proste wykresy - scatterplot
 plot(dane$Wiek, dane$HL)
-plot(dane$Dg, dane$SI)
+plot(dane$NPM, dane$SI)
+qqplot(dane$Wiek, dane$HL)
+
 
 #brak wartości - NA (Not Available)
 mean(dane$Wiek)
@@ -39,15 +41,45 @@ sd(dane$TPI200, na.rm = TRUE)
 mean(dane$TPI200, na.rm = TRUE)
 
 hist(dane$Wiek)
-hist()
+hist(dane$HL)
 
+d_wiek = density(dane$Wiek)
+plot(d_wiek)
 cor(dane)
 
 str(dane)
 #subset bazy danych
 sub_dane = dane[,c(3:4, 9:13)]
-cor(sub_dane)
-cor()
+cor(sub_dane, use = "complete.obs")
+cor(sub_dane ,method = "spearman")
+
+#iSTOTNOŚC KORELACJI
+cor.test(dane$Wiek, dane$HL)
+plot(dane$Wiek, dane$HL)
+
+
+
+#regresja liniowa
+reg_lin = lm(dane$HL ~ dane$Wiek)
+summary(reg_lin)
+
+plot(dane$HL, dane$SI)
+reg_lin2 = lm(dane$SI ~ dane$HL)
+summary(reg_lin3)
+loess_fit = loess(dane$HL ~ dane$SI)
+lines(dane$HL, predict(loess_fit), col = "blue")
+
+
+reg_lin3 = lm(dane$SI ~ dane$NPM)
+reg_nielin = lm(dane$SI ~ poly(dane$NPM,4))
+summary(reg_nielin)
+
+
+
+library(ggplot2)
+ggplot(dane, aes(dane$NPM, dane$SI)) + geom_point()+ geom_smooth(se=0)
+
+
 
 
 
