5146d99002e102e573dca39c226b50065ae6c80a
Ewa Grabska <egrabska@PC-0001.ur.local>
2019-12-11 13:14:31 +0100

    changes

--- skrypt1.R
+++ skrypt1.R
@@ -7,10 +7,15 @@
 log10(1000)
 cos(60) # w radianach!
 x = 3 #przypisanie wartości do obiektu x i y:
-y = 2
+y = 2.4
 x+y
 nazwa = "Wydział leśny"
 nazwa #wywołanie obiektu
+
+#TYPY OBIEKTÓW
+class()
+wektor = c(4, 8, 10)
+class(wektor)
 
 #funkcje
 #wczytywanie gotowej bazy danych, najczęściej w formacie .csv
@@ -19,6 +24,7 @@ dane = read.csv("D:/09_Dydaktyka/kurs_R/DL_dane_cw1.csv", sep = ";", dec = ",")
 str(dane)
 dane$Nachylenie
 dane[,4]
+dane[7,19]
 summary(dane)
 
 #proste wykresy - scatterplot
@@ -27,7 +33,21 @@ plot(dane$Dg, dane$SI)
 
 #brak wartości - NA (Not Available)
 mean(dane$Wiek)
+min(dane$TPI200)
+sd(dane$TPI200, na.rm = TRUE)
+
 mean(dane$TPI200, na.rm = TRUE)
 
 hist(dane$Wiek)
-?hist
+hist()
+
+cor(dane)
+
+str(dane)
+#subset bazy danych
+sub_dane = dane[,c(3:4, 9:13)]
+cor(sub_dane)
+cor()
+
+
+
