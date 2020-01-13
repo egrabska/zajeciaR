30591a3d27a7c626552ee7b1f1abf94bec1b7a4b
Ewa <you@example.com>
2020-01-13 19:19:38 +0100

    koncowe poprawki

--- konspekt2.md
+++ konspekt2.md
@@ -2,7 +2,7 @@ W ćwiczeniu wykorzystamy bazę danych ofert mieszkań w Krakowie pozyskaną
 z serwisu otodom między marcem a listopadem 2019 roku.
 
 Przed przystąpieniem do analiz należy wyczyścić bazę ze zbędnych kolumn
-i znaków oraz upewnić się, że wszytskie kolumny są odpowiedniego typu,
+i znaków oraz upewnić się, że wszystkie kolumny są odpowiedniego typu,
 tj. numeryczne typu *numeric*, kategorie (zmienne jakościowe) - typu
 *factor*, daty - format *date* itd.
 
@@ -14,15 +14,39 @@ tj. numeryczne typu *numeric*, kategorie (zmienne jakościowe) - typu
 Do "czyszczenia" zbędnych znaków można użyć funkcji `gsub()` lub
 `substr()`
 
--   sprawdź pomoc dla ww. funkcji i zastosuj je do danych
+-   Sprawdź pomoc dla ww. funkcji i zastosuj je do danych
 
-Ćwiczenie 1 Oblicz średnią, minimalną, maksymalną cenę za metr
-mieszkania w Krakowie Czy minimalne i maksymalne ceny na pewno są
-prawdziwymi wartościami? Utwórz boxploty (funkcja `boxplot()`) dla
-poszczególnych zmiennych. Przefiltruj dane - na podstawie zmiennej cena
-za metr, wybierz tylko te mieszkania, których cena za metr jest większa
-od 2000 i mniejsza od 30000 zł. Utwórz histogram cen za m2. Usuń
-duplikaty za pomocą funkcji `unique()`
+-   Użyj funkcji `as.numeric()`, `as.character()`, `as.date()`,
+    `as.factor()`
 
-Zainstaluj/wczytaj pakiet `dplyr` Przetestuj funkcje: `count()`,
-`filter()`, `arrange()`, `select()`, `mutate()`, `summarise()`
+-   Sprawdź wartości puste (*NA*) i usuń rekordy, które je zawierają
+    używając funkcji `na.omit()`
+
+-   Oblicz średnią, minimalną, maksymalną cenę za metr mieszkania w
+    Krakowie
+
+-   Czy minimalne i maksymalne ceny na pewno są prawdziwymi wartościami?
+
+-   Utwórz boxploty (funkcja `boxplot()`) dla poszczególnych zmiennych.
+
+-   Przefiltruj dane - na podstawie zmiennej cena za metr, wybierz tylko
+    te mieszkania, których cena za metr jest większa od 2000 i mniejsza
+    od 30000 zł.
+
+-   Usuń duplikaty za pomocą funkcji `unique()`
+
+-   Utwórz histogram cen za m2.
+
+------------------------------------------------------------------------
+
+W kolejnych krokach wykorzystamy narzędzia dostępne w pakiecie `dplyr`,
+który jest jednym z najpopularniejszych pakietów w przetwarzaniu danych,
+stosowany aby poprawić czytelnośc i lepiej je zorganizować (*data
+manipulation*)
+
+-   Zainstaluj/wczytaj pakiet `dplyr`
+
+-   Przetestuj funkcje: `glimpse()`, `count()`, `filter()`, `arrange()`,
+    `select()`, `mutate()`, `summarise()`, `group_by()`
+
+-   Oblicz średnie ceny za metr dla dzielnic i dla miesięcy.
--- skrypt2.R
+++ skrypt2.R
@@ -1,4 +1,4 @@
-offers = read.csv("D:/09_Dydaktyka/kurs_R/offers3.csv", sep = ";")
+offers = read.csv("c:/04_R/zajeciaR/offers3.csv", sep = ";")
 
 
 #sprawdzamy co to za dane - funkcja str() pokazuje nam strukture obiektu
@@ -24,6 +24,7 @@ summary(oferty_sub)
 #czyszczenie danych z niepotrzebnych zanków, przerw itp. 
 #wszystkie zbędne litery w kolumnach 2:5 - funkcje gsub i substr
 ?gsub
+?substr
 
 oferty_sub$cena_za_metr = gsub(" /", "", oferty_sub$cena_za_metr)
 oferty_sub$cena_za_metr = gsub(" ", "", oferty_sub$cena_za_metr)
@@ -31,14 +32,14 @@ oferty_sub$pokoje = substr(oferty_sub$pokoje, 1,1)
 oferty_sub$dzielnica = gsub(": ", "", oferty_sub$dzielnica)
 oferty_sub$metraz = gsub(",", ".", oferty_sub$metraz)
 
+#zamiana typów kolumn na liczbowe - wykorzystanie lapply
 #zamiana zmienej data na typ daty 
 
 str(oferty_sub)
+
 oferty_sub$cena = as.numeric(as.character(oferty_sub$cena))
-#zamiana typów kolumn na liczbowe - wykorzystanie lapply
 oferty_sub[c(2,4,5)] = lapply(oferty_sub[c(2,4,5)], as.numeric)
 oferty_sub$data = as.Date(oferty_sub$data, "%Y-%m-%d")
-
 
 #wartości NA
 sum(is.na(oferty_sub))
@@ -55,7 +56,6 @@ boxplot(oferty_sub$cena)
 
 oferty_sub = oferty_sub[oferty_sub$cena_za_metr > 2000 & oferty_sub$cena_za_metr < 30000,]
 
-
 boxplot(oferty_sub$cena_za_metr)
 boxplot(oferty_sub$cena)
 
@@ -66,41 +66,43 @@ oferty_sub = unique(oferty_sub)
 
 
 
-#-------------------------------------------
+#----------wykorzystanie pakiet dplyr
 library(dplyr)
+glimpse(oferty_sub)
 ile = count(oferty_sub, dzielnica)
 ile$n
 dzielnice = ile[ile$n > 20,]
 
-dane_20 = filter(oferty_sub, dzielnica %in% dzielnice$dzielnica & dzielnica != "małopolskie")
+#filtrowanie danych 
+
 filter(dane_20, dane_20$pokoje == 4)
 filter(dane_20, pokoje == 4 & metraz < 100)
 czteropokojowe = filter(dane_20, pokoje == 4)
-
+dane_20 = filter(oferty_sub, dzielnica %in% dzielnice$dzielnica & dzielnica != "małopolskie")
 x = filter(dane_20, dzielnica %in% c("Krowodrza", "Olsza", "Łobzów"))
+
 #arrange 
 arrange(x,desc(metraz))
 #select - wybranie po nazwach kolumn
-new_offers = select(dane_20, cena, metraz, dzielnica)
+nowy_df = select(dane_20, cena, metraz, dzielnica)
 
 #mutate - utworzenie nowej zmiennej na podstawie innych zmiennych
-new_offers = mutate(new_offers, cena_za_m = cena/metraz)
+nowy_df = mutate(nowy_df, cena_za_m = cena/metraz)
 
 
 
 #group_by i summarise
-summarise(dane_20, mean(cena))
+summarise(oferty_sub, mean(cena_za_metr))
 
 
-groups = group_by(dane_20, dzielnica) #wygląda tak samo
+groups = group_by(oferty_sub, dzielnica) #wygląda tak samo
 
-sr_krk = summarise(dane_20, sr_cena_za_m = mean(cena_za_metr))
-
-srednie_dzielnice = summarise(group_by(dane_20, dzielnica), sr_cena_za_m = mean(cena_za_metr))
-
-miesiac = format(dane_20$data,"%m")
-dane_20$miesiac = miesiac
-srednie_miesiac = summarise(group_by(dane_20, miesiac), sr_cena_za_m = mean(cena_za_metr))
+oferty_sub$miesiac = miesiac
+srednie_miesiac = summarise(group_by(oferty_sub, miesiac), sr_cena_za_m = mean(cena_za_metr))
 
 plot(srednie_miesiac, type = "l")
+
+
+sr_krk = summarise(oferty_sub, sr_cena_za_m = mean(cena_za_metr))
+srednie_dzielnice = summarise(group_by(oferty_sub, dzielnica), sr_cena_za_m = mean(cena_za_metr))
 
